d40d1b2ea1be3204de24b0bf88c8fc3c13de6403
rafactor <avila.rafael@gmail.com>
2018-12-12 16:58:30 -0500

    word.js completed

--- letter.js
+++ letter.js
@@ -7,30 +7,10 @@ var Letter = function(letter) {
     this.validate = function (guess) {
         if (this.letter === guess) {
             this.guessed = true;
-            console.log('right')
         } else {
-            console.log('wrong')
+            // console.log('this letter:' + this.letter + '| guessed: ' + guess)
         }
     }
 }
-
-// Letter.prototype.display = function () {
-//     return (this.guessed === true) ? this.letter : '_';
-// }
-
-// Letter.prototype.validate = function (guess) {
-//     if (this.letter === guess) {
-//         this.guessed = true;
-//         console.log('right')
-//     } else {
-//         console.log('wrong')
-//     }
-// }
-
-// var test = new Letter('b')
-// test.validate('c')
-// var x = test.display()
-// console.log(x)
-
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,10 +1,52 @@
 var Letter = require("./letter.js")
-
-// var test = new Letter('b')
-// test.validate('b')
-// var x = test.display()
-// console.log(x)
+var objects = [];
+var userDisplay ="";
 
 var Word = function(word){
+    this.word = word,
+    this.build = () =>{
+        let counter = 0;
+        for (let o of this.word){
+            objects[counter] = new Letter(o)
+            counter += 1;
+        }
+        this.display();
+    },
+
+    this.display = function(){
+        userDisplay = 'word to guess: '
+        let counter = 0;
+        for (let o of this.word){     
+            let guessed = objects[counter].display();
+            counter += 1;
+            userDisplay += guessed + ' '
+        }
+        
+        console.log(userDisplay)
     
-}
+    },
+
+
+    this.guess = function(letter){
+        let counter = 0;
+        for (let o of this.word){
+            objects[counter].validate(letter);
+            // console.log(objects[counter])
+            counter +=1
+        }
+        this.display();
+
+    }
+} 
+
+module.exports = Word;
+
+// var test = new Word('rafael');
+
+// test.build()
+// test.guess('a')
+// test.guess('e')
+// test.guess('r')
+// test.guess('k')
+// test.guess('l')
+
