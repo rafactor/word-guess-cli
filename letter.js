d4d9a53a04bc5969ef2a5198c7a874949329ab00
rafactor <avila.rafael@gmail.com>
2018-12-13 08:47:01 -0500

    update code to ES6

--- letter.js
+++ letter.js
@@ -1,14 +1,14 @@
-var Letter = function(letter) {
-    this.letter = letter,
-    this.guessed = false,
-    this.display = function () {
-        return (this.guessed === true) ? this.letter : '_';
-    },
-    this.validate = function (guess) {
-        if (this.letter === guess) {
-            this.guessed = true;
-        } else {
-            // console.log('this letter:' + this.letter + '| guessed: ' + guess)
+class Letter {
+    constructor(letter){
+        this.letter = letter,
+        this.guessed = false,
+        this.display = () => {
+            return (this.guessed === true) ? this.letter : '_';
+        },
+        this.validate = (guess) => {
+            if (this.letter === guess) {
+                this.guessed = true;
+            }
         }
     }
 }
--- word.js
+++ word.js
@@ -2,51 +2,47 @@ var Letter = require("./letter.js")
 var objects = [];
 var userDisplay ="";
 
-var Word = function(word){
-    this.word = word,
-    this.build = () =>{
-        let counter = 0;
-        for (let o of this.word){
-            objects[counter] = new Letter(o)
-            counter += 1;
-        }
-        this.display();
-    },
-
-    this.display = function(){
-        userDisplay = 'word to guess: '
-        let counter = 0;
-        for (let o of this.word){     
-            let guessed = objects[counter].display();
-            counter += 1;
-            userDisplay += guessed + ' '
-        }
-        
-        console.log(userDisplay)
+class Word {
+    constructor(word) {
+        this.word = word;
+        this.build = () =>{
+            let counter = 0;
+            for (let o of this.word){
+                objects[counter] = new Letter(o)
+                counter += 1;
+            }
+            this.display();
+        },
     
-    },
-
-
-    this.guess = function(letter){
-        let counter = 0;
-        for (let o of this.word){
-            objects[counter].validate(letter);
-            // console.log(objects[counter])
-            counter +=1
+        this.display = () => {
+            userDisplay = 'word to guess: '
+            let counter = 0;
+            for (let o of this.word){     
+                let guessed = objects[counter].display();
+                counter += 1;
+                userDisplay += guessed + ' '
+            }
+            console.log(userDisplay)
+        },    
+        this.guess = (letter) =>{
+            let counter = 0;
+            for (let o of this.word){
+                objects[counter].validate(letter);
+                counter +=1
+            }
+            this.display();
         }
-        this.display();
-
     }
-} 
+}
 
 module.exports = Word;
 
-// var test = new Word('rafael');
+var test = new Word('rafael');
 
-// test.build()
-// test.guess('a')
-// test.guess('e')
-// test.guess('r')
-// test.guess('k')
-// test.guess('l')
+test.build()
+test.guess('a')
+test.guess('e')
+test.guess('r')
+test.guess('k')
+test.guess('l')
 
